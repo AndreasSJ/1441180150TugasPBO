96c6e48ea2cbe9aee35eea5d8857aabd9495dc6c
AndreasSJ <andreassuryajaya6@gmail.com>
2015-03-26 23:02:18 +0700

    andre2
    tugas pbo

---
+++ Tugas2/soal2.java
@@ -0,0 +1,13 @@
+public class soal2 {
+	public static void main(String[] args) {
+		int angka = 18;
+			
+		if (angka% 2 == 0) {
+			System.out.println("genap");
+		}
+		else {
+			System.out.println("ganjil");
+		}
+	}
+	
+}
---
+++ Tugas2/soal5.java
@@ -0,0 +1,12 @@
+public class  soal5{
+	public static void main(String[] args) {
+
+		int b;
+		for (b = 1;b <= 10 ;b++ ) 
+			System.out.print(b);	
+		
+		if (b<10) {
+			System.out.print(",");
+		}
+	}
+}
